055de5b2e74259d4d82202b8ae134110ba5b441b
İsmet Kizgin <info@ismetkizgin.com>
2021-04-17 02:48:53 +0300

    fix: SQL Injection turn off the deficit

--- src/utils/queryUtils.ts
+++ src/utils/queryUtils.ts
@@ -1,8 +1,12 @@
+import Mysql from 'mysql';
+
 export const getWhere = (object: object) => {
   let where = 'WHERE ';
   for (const [key, value] of Object.entries(object))
     where +=
-      where === 'WHERE ' ? `${key}='${value}'` : ` and ${key}='${value}'`;
+      where === 'WHERE '
+        ? `${Mysql.escapeId(key)}=${Mysql.escape(value)}`
+        : ` and ${Mysql.escapeId(key)}=${Mysql.escape(value)}`;
 
   return where === 'WHERE ' ? '' : where;
 };
